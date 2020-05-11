6b15a7da70dc08c0e3ebcdebc1fec0bb79e0f906
sandeulsandeul <wjdtksdk@ewhain.net>
2020-05-11 17:32:34 +0900

    comboBox

--- WindowsFormsApp2/Form1.Designer.cs
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace WindowsFormsApp2
+﻿using System.Windows.Forms;
+
+namespace WindowsFormsApp2
 {
     partial class Form1
     {
@@ -28,63 +30,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
-            // button1
+            // comboBox1
             // 
-            this.button1.Location = new System.Drawing.Point(145, 125);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(152, 50);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.comboBox1.DropDownStyle = ComboBoxStyle.Simple;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(57, 101);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 26);
+            this.comboBox1.TabIndex = 0;
             // 
-            // button2
+            // comboBox2
             // 
-            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
-            this.button2.Location = new System.Drawing.Point(234, 196);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(152, 50);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.comboBox2.DropDownStyle = ComboBoxStyle.DropDown;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(234, 101);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(178, 26);
+            this.comboBox2.TabIndex = 1;
             // 
-            // button3
+            // comboBox3
             // 
-            this.button3.Image = global::WindowsFormsApp2.Properties.Resources.button_;
-            this.button3.Location = new System.Drawing.Point(54, 196);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(152, 50);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.comboBox3.DropDownStyle = ComboBoxStyle.DropDownList;
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(448, 101);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(152, 26);
+            this.comboBox3.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(430, 450);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(688, 450);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
 
+
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
     }
 }
 
--- WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/Form1.cs
@@ -18,22 +18,47 @@ namespace WindowsFormsApp2
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void Form1_Load(object sender, EventArgs e)
         {
-            MessageBox.Show("Button1 is Clicked");
-            button1.BackColor = Color.Cyan;
-            button1.ForeColor = Color.Blue;
+            // 콤보박스에 사용할 데이터 선언 
+            string[] data = {"사과", "토마토", "포도","배","복숭아" };
+
+            //comboBox1 은 SImple
+            //comboBox2 은 DropDown
+            //comboBox3 은 DropDownList
+
+            // 각 톰보 박스에 데이터 초기화
+            comboBox1.Items.AddRange(data);
+            comboBox2.Items.AddRange(data);
+            comboBox3.Items.AddRange(data);
+
+            // 처음 선택값 지정, 첫번째 아이템 선택  (사과)
+            comboBox1.SelectedIndex = 0;
+            comboBox2.SelectedIndex = 0;
+            comboBox3.SelectedIndex = 0;
+
+            // simple 콤보 박스의 목록을 초기화하고 다시 목록 추가
+            /*
+             * comboBox1.Items.Add("Item 1");
+             * comboBox1.Items.Clear();
+             * comboBox1.Items.AddRange( new String[] {"1","2","3"} );
+             */
+
+
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        //SelectedIndexChanged 이벤트 헨들링
+        // 사용자가 콤보 박스의 목록 중에서 다른 아이템을 선택시 발생
+
+        private void comboDropDown_SelectedIndexChanged(object sender, EventArgs e)
         {
-            MessageBox.Show("Button2 is Clicked");
+            if(comboBox2.SelectedIndex >= 0)
+            {
+                this.itemSelected = comboBox2.SelectedItem as string; 
+            }
         }
 
-        private void button3_Click(object sender, EventArgs e)
-        {
-            MessageBox.Show("Button3 is Clicked");
-        }
+        private string itemSelected;
     }
 }
 

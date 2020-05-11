dd7addf72a9acca4d6ccf047f96713580f40937c
sandeulsandeul <wjdtksdk@ewhain.net>
2020-05-12 02:38:03 +0900

    simple todolist

--- WindowsFormsApp2/Form1.Designer.cs
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -30,142 +30,151 @@ namespace WindowsFormsApp2
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Plus = new System.Windows.Forms.Button();
+            this.taskinput = new System.Windows.Forms.TextBox();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.todolist = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.Delete = new System.Windows.Forms.Button();
             this.listView1 = new System.Windows.Forms.ListView();
-            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
-            this.listView2 = new System.Windows.Forms.ListView();
-            this.imageList2 = new System.Windows.Forms.ImageList(this.components);
-            this.radioDetails = new System.Windows.Forms.RadioButton();
-            this.radioList = new System.Windows.Forms.RadioButton();
-            this.radioTitle = new System.Windows.Forms.RadioButton();
-            this.radioButton4 = new System.Windows.Forms.RadioButton();
-            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.todolist.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // Plus
+            // 
+            this.Plus.Location = new System.Drawing.Point(462, 50);
+            this.Plus.Name = "Plus";
+            this.Plus.Size = new System.Drawing.Size(71, 28);
+            this.Plus.TabIndex = 0;
+            this.Plus.Text = "+";
+            this.Plus.UseVisualStyleBackColor = true;
+            this.Plus.Click += new System.EventHandler(this.Plus_Click);
+            // 
+            // taskinput
+            // 
+            this.taskinput.Location = new System.Drawing.Point(7, 50);
+            this.taskinput.Name = "taskinput";
+            this.taskinput.Size = new System.Drawing.Size(452, 28);
+            this.taskinput.TabIndex = 3;
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(7, 84);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(528, 454);
+            this.checkedListBox1.TabIndex = 5;
+            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // todolist
+            // 
+            this.todolist.Controls.Add(this.tabPage1);
+            this.todolist.Controls.Add(this.tabPage2);
+            this.todolist.Location = new System.Drawing.Point(-3, 1);
+            this.todolist.Name = "todolist";
+            this.todolist.SelectedIndex = 0;
+            this.todolist.Size = new System.Drawing.Size(555, 581);
+            this.todolist.TabIndex = 7;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.textBox2);
+            this.tabPage1.Controls.Add(this.checkedListBox1);
+            this.tabPage1.Controls.Add(this.Plus);
+            this.tabPage1.Controls.Add(this.taskinput);
+            this.tabPage1.Location = new System.Drawing.Point(4, 28);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(547, 549);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(7, 16);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(527, 28);
+            this.textBox2.TabIndex = 6;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.textBox3);
+            this.tabPage2.Controls.Add(this.Delete);
+            this.tabPage2.Controls.Add(this.listView1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 28);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(547, 549);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(5, 21);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(527, 28);
+            this.textBox3.TabIndex = 7;
+            // 
+            // Delete
+            // 
+            this.Delete.Location = new System.Drawing.Point(451, 507);
+            this.Delete.Name = "Delete";
+            this.Delete.Size = new System.Drawing.Size(81, 26);
+            this.Delete.TabIndex = 5;
+            this.Delete.Text = "Del";
+            this.Delete.UseVisualStyleBackColor = true;
+            this.Delete.Click += new System.EventHandler(this.Delete_Click_1);
             // 
             // listView1
             // 
+            this.listView1.AllowDrop = true;
+            this.listView1.CheckBoxes = true;
             this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.Location = new System.Drawing.Point(6, 55);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(505, 426);
-            this.listView1.SmallImageList = this.imageList1;
+            this.listView1.Size = new System.Drawing.Size(527, 435);
             this.listView1.TabIndex = 0;
             this.listView1.UseCompatibleStateImageBehavior = false;
-            // 
-            // imageList1
-            // 
-            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
-            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
-            this.imageList1.Images.SetKeyName(0, "file_image.jpg");
-            // 
-            // listView2
-            // 
-            this.listView2.HideSelection = false;
-            this.listView2.Location = new System.Drawing.Point(523, 12);
-            this.listView2.Name = "listView2";
-            this.listView2.Size = new System.Drawing.Size(229, 426);
-            this.listView2.TabIndex = 1;
-            this.listView2.UseCompatibleStateImageBehavior = false;
-            // 
-            // imageList2
-            // 
-            this.imageList2.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList2.ImageStream")));
-            this.imageList2.TransparentColor = System.Drawing.Color.Transparent;
-            this.imageList2.Images.SetKeyName(0, "file_image.jpg");
-            // 
-            // radioDetails
-            // 
-            this.radioDetails.AutoSize = true;
-            this.radioDetails.Location = new System.Drawing.Point(541, 37);
-            this.radioDetails.Name = "radioDetails";
-            this.radioDetails.Size = new System.Drawing.Size(125, 22);
-            this.radioDetails.TabIndex = 2;
-            this.radioDetails.TabStop = true;
-            this.radioDetails.Text = "radioDetails";
-            this.radioDetails.UseVisualStyleBackColor = true;
-            this.radioDetails.CheckedChanged += new System.EventHandler(this.radioDetails_CheckedChanged_1);
-            // 
-            // radioList
-            // 
-            this.radioList.AutoSize = true;
-            this.radioList.Location = new System.Drawing.Point(541, 79);
-            this.radioList.Name = "radioList";
-            this.radioList.Size = new System.Drawing.Size(100, 22);
-            this.radioList.TabIndex = 3;
-            this.radioList.TabStop = true;
-            this.radioList.Text = "radioList";
-            this.radioList.UseVisualStyleBackColor = true;
-            this.radioList.CheckedChanged += new System.EventHandler(this.radioList_CheckedChanged_1);
-            // 
-            // radioTitle
-            // 
-            this.radioTitle.AutoSize = true;
-            this.radioTitle.Location = new System.Drawing.Point(541, 124);
-            this.radioTitle.Name = "radioTitle";
-            this.radioTitle.Size = new System.Drawing.Size(104, 22);
-            this.radioTitle.TabIndex = 4;
-            this.radioTitle.TabStop = true;
-            this.radioTitle.Text = "radioTitle";
-            this.radioTitle.UseVisualStyleBackColor = true;
-            this.radioTitle.CheckedChanged += new System.EventHandler(this.radioTitle_CheckedChanged_1);
-            // 
-            // radioButton4
-            // 
-            this.radioButton4.AutoSize = true;
-            this.radioButton4.Location = new System.Drawing.Point(541, 166);
-            this.radioButton4.Name = "radioButton4";
-            this.radioButton4.Size = new System.Drawing.Size(135, 22);
-            this.radioButton4.TabIndex = 5;
-            this.radioButton4.TabStop = true;
-            this.radioButton4.Text = "radioButton4";
-            this.radioButton4.UseVisualStyleBackColor = true;
-            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
-            // 
-            // radioButton5
-            // 
-            this.radioButton5.AutoSize = true;
-            this.radioButton5.Location = new System.Drawing.Point(541, 217);
-            this.radioButton5.Name = "radioButton5";
-            this.radioButton5.Size = new System.Drawing.Size(135, 22);
-            this.radioButton5.TabIndex = 6;
-            this.radioButton5.TabStop = true;
-            this.radioButton5.Text = "radioButton5";
-            this.radioButton5.UseVisualStyleBackColor = true;
-            this.radioButton5.CheckedChanged += new System.EventHandler(this.radioButton5_CheckedChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(764, 450);
-            this.Controls.Add(this.radioButton5);
-            this.Controls.Add(this.radioButton4);
-            this.Controls.Add(this.radioTitle);
-            this.Controls.Add(this.radioList);
-            this.Controls.Add(this.radioDetails);
-            this.Controls.Add(this.listView2);
-            this.Controls.Add(this.listView1);
+            this.ClientSize = new System.Drawing.Size(545, 574);
+            this.Controls.Add(this.todolist);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.todolist.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
 
         #endregion
 
+        private Button Plus;
+        private TextBox taskinput;
+        private CheckedListBox checkedListBox1;
+        private TabControl todolist;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
         private ListView listView1;
-        private ListView listView2;
-        private ImageList imageList1;
-        private ImageList imageList2;
-        private RadioButton radioDetails;
-        private RadioButton radioList;
-        private RadioButton radioTitle;
-        private RadioButton radioButton4;
-        private RadioButton radioButton5;
+        private Button Delete;
+        private TextBox textBox2;
+        private TextBox textBox3;
     }
 }
 
--- WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/Form1.cs
@@ -19,84 +19,62 @@ namespace WindowsFormsApp2
         {
             InitializeComponent();
         }
+        // 변수 설정 
+        int tasknum = 0;
+        int completenum = 0;
+
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            /*
-             * 현재 디렉토리 내의 파일리스트 얻기
-             * using System.IO;가 필수로 사용된다. 
-             *
-             */
+            textBox2.ReadOnly = true;
+            textBox3.ReadOnly = true;
+        }
+        //item 추가 
+        private void Plus_Click(object sender, EventArgs e)
+        {
+            string str = taskinput.Text;
+            checkedListBox1.Items.Add(str);
+            taskinput.Text = "";
+            tasknum += 1;
 
-            // currdir에 현재 파일이 저장될 위치 
-            string currdir = Environment.CurrentDirectory;
-            // new DirectoryInfo(currdir) 를 통해서
-            // currdir의 정보를 읽어들인다. 
-            DirectoryInfo di = new DirectoryInfo(currdir);
-            // di.GetFiles()를 통해 di가 가진 파일들을 
-            // files에 담는다. 
-            FileInfo[] files = di.GetFiles();
+            textBox2.Text = tasknum + "개의 할 일이 있습니다.";
+            textBox3.Text = completenum + "개의 일을 완료했습니다.";
+        }
 
-            //리스트 뷰 아이이템 업데이트 시작 
-            // 업데이트가 종료될 때까지 ui 갱신 중지.
-            listView1.BeginUpdate();
 
-            // 뷰 모드 지정
-            listView1.View = View.Details;
-
-            // 아이콘을 위해 이미지 지정
-            listView1.LargeImageList = imageList1;
-            listView1.SmallImageList = imageList2;
-
-            foreach(var fi in files)
+        // task 완료  
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            foreach (var r in checkedListBox1.CheckedItems.OfType<string>().ToList())
             {
-                //각 파일별로 ListViewItem 객체를 하나씩 만들기
-                // 파일명, 사이즈, 날짜 정보를 추가함.
+                listView1.Items.Add(r);
+                checkedListBox1.Items.Remove(r);
+                tasknum -= 1;
+                completenum++;
 
-                ListViewItem lvi = new ListViewItem(fi.Name);
-                lvi.SubItems.Add(fi.Length.ToString());
-                lvi.SubItems.Add(fi.LastWriteTime.ToString());
-                lvi.ImageIndex = 0;
-
-                //ListViewItem 객체를 Items 속성에 추가하기
-                listView1.Items.Add(lvi);
-
+                textBox2.Text = tasknum + "개의 할 일이 있습니다.";
+                textBox3.Text = completenum + "개의 일을 완료했습니다.";
             }
-            // 컬럼명과 컬럼 사이즈를 지정
-            listView1.Columns.Add("파일명", 200, HorizontalAlignment.Left);
-            listView1.Columns.Add("사이즈", 70, HorizontalAlignment.Left);
-            listView1.Columns.Add("날짜", 100, HorizontalAlignment.Left);
-
-            //리스트뷰를 새로고침하여 보여줌
-            listView1.EndUpdate();
         }
 
-        private void radioDetails_CheckedChanged_1(object sender, EventArgs e)
+        // task 삭제 
+        private void Delete_Click_1(object sender, EventArgs e)
         {
-            listView1.View = View.Details;
-        }
-
-        private void radioTitle_CheckedChanged_1(object sender, EventArgs e)
-        {
-            listView1.View = View.Tile;
-        }
-
-        private void radioButton4_CheckedChanged(object sender, EventArgs e)
-        {
-            listView1.View = View.LargeIcon;
-        }
-        //View 모드를 지정
+            for (int i = 0 ; i < listView1.Items.Count ; i++)
+            {
+                if(listView1.Items[i].Checked == true)
+                {
+                    listView1.Items.RemoveAt(i);
+                    completenum--;
 
 
-        private void radioButton5_CheckedChanged(object sender, EventArgs e)
-        {
-            listView1.View = View.SmallIcon;
+
+                    textBox3.Text = completenum + "개의 일을 완료했습니다.";
+                }
+            }
+            
         }
 
-        private void radioList_CheckedChanged_1(object sender, EventArgs e)
-        {
-            listView1.View = View.List;
-        }
     }
 }
 

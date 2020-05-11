08b50a35b4a0d1c70b5a1074117e6cd62626ed6e
sandeulsandeul <wjdtksdk@ewhain.net>
2020-05-11 14:59:20 +0900

    checkbox

--- WindowsFormsApp2/Form1.Designer.cs
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -28,75 +28,99 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // button1
+            // checkBox1
             // 
-            this.button1.Location = new System.Drawing.Point(616, 44);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(110, 145);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(32, 94);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(125, 22);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Checked = true;
+            this.checkBox2.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox2.Location = new System.Drawing.Point(278, 94);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(125, 22);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "checkBox2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Checked = true;
+            this.checkBox3.CheckState = System.Windows.Forms.CheckState.Indeterminate;
+            this.checkBox3.Location = new System.Drawing.Point(165, 174);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(125, 22);
+            this.checkBox3.TabIndex = 2;
+            this.checkBox3.Text = "checkBox3";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(165, 215);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(125, 22);
+            this.checkBox4.TabIndex = 3;
+            this.checkBox4.Text = "checkBox4";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(165, 261);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(125, 22);
+            this.checkBox5.TabIndex = 4;
+            this.checkBox5.Text = "checkBox5";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Location = new System.Drawing.Point(165, 335);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(125, 22);
+            this.checkBox6.TabIndex = 5;
+            this.checkBox6.Text = "checkBox6";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            this.checkBox6.CheckedChanged += new System.EventHandler(this.checkBox6_CheckedChanged);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(35, 44);
-            this.textBox1.Multiline = true;
+            this.textBox1.Location = new System.Drawing.Point(165, 372);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(566, 145);
-            this.textBox1.TabIndex = 1;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(35, 207);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(566, 72);
-            this.textBox2.TabIndex = 2;
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(35, 285);
-            this.textBox3.Multiline = true;
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(566, 28);
-            this.textBox3.TabIndex = 3;
-            // 
-            // textBox4
-            // 
-            this.textBox4.Location = new System.Drawing.Point(35, 319);
-            this.textBox4.Multiline = true;
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(566, 56);
-            this.textBox4.TabIndex = 4;
-            // 
-            // textBox5
-            // 
-            this.textBox5.Location = new System.Drawing.Point(35, 392);
-            this.textBox5.Multiline = true;
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(566, 33);
-            this.textBox5.TabIndex = 5;
+            this.textBox1.Size = new System.Drawing.Size(100, 28);
+            this.textBox1.TabIndex = 6;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
+            this.ClientSize = new System.Drawing.Size(430, 450);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox6);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load_1);
@@ -107,12 +131,13 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox6;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox5;
     }
 }
 
--- WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/Form1.cs
@@ -15,47 +15,38 @@ namespace WindowsFormsApp2
 
         public Form1()
         {
-            /*public Form1 이라는 창 화면을 
-              InitializeComponent();함수를 적용해
-              디자인한 화면으로적용*/
             InitializeComponent();
-
-            /*button1이 클릭되었을 때 button_Click() 함수 적용 */
-            this.button1.Click += new System.EventHandler(this.button1_Click_1);
-
-           
-
         }
 
-        /*private (클래스 내에서만 접근 가능 )로 button1_Click 함수 생성*/
-
-        private void button1_Click_1(object sender, EventArgs e)
+        private void Form1_Load_1(object sender, EventArgs e)
         {
-            /*MessageBox의 Show 사용해 Hello World 출력 */
-            MessageBox.Show("Hello World");
+            //Check 상태로 초기화하기
+            checkBox1.Checked = true;
+
+            // UnCheked 상태로 초기화하기
+            checkBox2.Checked = false;
+
+            //Intwemediate 상태로 초기화하기 
+            //Form1.cs[디자인] 에서 마우스 오른쪽 클릭 후 속성
+            // 모양에서 CheckState에서 Intermediate  선택 
+
+            // Intermediate 에 속한 4번 체크 박스 checked 로 초기화
+            checkBox4.Checked = true;
+
+            // Intermediate 에 속한 5번 체크 박스 Unchecked 로 초기화
+            checkBox5.Checked = false;
+
+            // checkBox6의 체크상태가 바뀔 때마다  checkBox6_CheckStateChanged 함수 호출
+            checkBox6.CheckStateChanged += checkBox6_CheckedChanged;
         }
 
-        private void Form1_Load_1( object sender, EventArgs e)
+        int count = 0;
+        private void checkBox6_CheckedChanged(object sender, EventArgs e)
         {
-            //TextBox에 초기값 할당
-            textBox1.Text = "서울특별시 영등포구 신길동 123번지";
-
-            StringBuilder sb = new StringBuilder();
-            sb.AppendLine("이름 : 홍길동");
-            sb.AppendLine("나이 : 27세");
-            sb.AppendLine("국적 : 한국");
-            textBox2.Text = sb.ToString();
-
-            /*최대 길이 지정*/
-            textBox3.MaxLength = 10;
-            /*읽기만 가능 */
-            textBox4.ReadOnly = true;
-            /* PasswordChar 는 비밀번호를 받는다*/
-            textBox5.PasswordChar = '*'; 
-
-            textBox3.Text = "서울특별시 영등포구 신길동 123번지";
-            textBox4.Text = "서울특별시 영등포구 신길동 123번지";
-            textBox5.Text = "서울특별시 영등포구 신길동 123번지";
+            // 텍스트 박스에 변경 시마다 변경 횟수+ 변경이라고 적기
+            count ++ ;
+            textBox1.ReadOnly = true;
+            textBox1.Text = count + "변경";
         }
     }
 }
